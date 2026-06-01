40bd7c9a40b24cbf5fe5633ffa6d6826506cc654
nathnatsu54-cmd <nathnatsu54@gmail.com>
2026-06-01 08:35:36 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,14 @@ class $modify(PlayLayer) {
         // 1. On laisse le jeu faire son calcul normal d'abord
         PlayLayer::updateProgressbar();
 
-        // 2. On vérifie si le label du pourcentage existe pour éviter les crashs
-        if (this->m_percentageLabel) {
+        // 2. On utilise le bon nom : m_percentLabel
+        if (this->m_percentLabel) {
             
             // 3. On vérifie si on est en mode Pratique OU en Testmode
-            // La fonction isTestMode() fait partie de PlayLayer
             bool shouldHide = this->m_isPracticeMode || this->isTestMode();
 
-            if (shouldHide) {
-                this->m_percentageLabel->setVisible(false);
-            } 
-            else {
-                this->m_percentageLabel->setVisible(true);
-            }
+            // 4. On applique la visibilité
+            this->m_percentLabel->setVisible(!shouldHide);
         }
     }
 };
