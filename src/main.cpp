794aeec2c36e3df48592be1ca37a4c5b51e5b3b7
nathnatsu54-cmd <nathnatsu54@gmail.com>
2026-06-01 08:27:37 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,22 @@
 using namespace geode::prelude;
 
 class $modify(PlayLayer) {
+    
     void updateProgressbar() {
+        // 1. On laisse le jeu faire son calcul normal d'abord
         PlayLayer::updateProgressbar();
 
-        if (this->m_isPracticeMode) {
-            if (this->m_percentageLabel) {
+        // 2. On vérifie si le label du pourcentage existe pour éviter les crashs
+        if (this->m_percentageLabel) {
+            
+            // 3. On vérifie si on est en mode Pratique OU en Testmode
+            // La fonction isTestMode() fait partie de PlayLayer
+            bool shouldHide = this->m_isPracticeMode || this->isTestMode();
+
+            if (shouldHide) {
                 this->m_percentageLabel->setVisible(false);
-            }
-        } else {
-            if (this->m_percentageLabel) {
+            } 
+            else {
                 this->m_percentageLabel->setVisible(true);
             }
         }
